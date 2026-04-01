2a89e6c5035c4490fac09c2b456dc3a7593c4b55
rrajbirkaur2 <rrajbirkaur2@rrc.ca>
2026-04-01 14:57:20 -0500

    Add Active Admin pages resource for editable about and contact pages

---
+++ app/admin/pages.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register Page do
+  permit_params :title, :content, :slug
+
+  actions :all, except: [:new, :destroy]
+
+  index do
+    id_column
+    column :slug
+    column :title
+    actions
+  end
+
+  filter :slug
+  filter :title
+
+  form do |f|
+    f.inputs "Edit Page Content" do
+      f.input :slug, input_html: { disabled: true }
+      f.input :title
+      f.input :content, as: :text, input_html: { rows: 10 }
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :slug
+      row :title
+      row :content
+    end
+  end
+end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,13 @@
 class Page < ApplicationRecord
+  validates :slug, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["content", "created_at", "id", "slug", "title", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
